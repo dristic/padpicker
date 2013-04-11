3d2e0ef2a6850a7008cfebb678b28f2f4566894f
dristic <dristic101@gmail.com>
2013-04-11 12:08:51 -0700

    Add basic picker and button.

---
+++ src/picker.js
@@ -0,0 +1,35 @@
+function pick(success, failure) {
+  filepicker.pick({
+      mimetypes: ['text/plain'],
+      container: 'window',
+      services:['COMPUTER', 'GMAIL', 'BOX', 'DROPBOX', 'GITHUB'],
+    },
+    function(FPFile){
+      console.log(JSON.stringify(FPFile));
+      success(FPFile);
+    },
+    function(FPError){
+      console.log(FPError.toString());
+      failure(FPError);
+    }
+  );
+}
+
+$(document).ready(function () {
+  var chooseAFileButton = $('#choose-a-file-btn');
+  var pickError = $('#pick-error');
+
+  pickError.hide();
+
+  chooseAFileButton.click(function () {
+    pick(function (file) {
+      $(document).trigger('file-picked', file);
+    }, function (error) {
+      pickError.show();
+    });
+  });
+
+  $(document).on('file-picked', function (data, file) {
+    console.log(data, file);
+  });
+});
