806092bf1eb60ef02be7df3f24b21d7096c1cd0c
Brett van Zuiden <brettcvz@gmail.com>
2013-04-11 14:53:17 -0700

    Added hash functionality

--- src/padpicker.js
+++ src/padpicker.js
@@ -1,23 +1,54 @@
 $(function(){
-   var pad = padpicker.create('firepad');
+   //Checking hash to see if we need to show welcome
+   var hash = getHash();
+   if (!hash) {
+       $(document).trigger("show-welcome");
+       hash = createGuid();
+       window.location.hash = hash;
+   }
+   var pad = padpicker.create('firepad', hash);
 
    $(document).on("file-picked", function(e, fpfile){
 	pad.setFile(fpfile);
    });
+
+   var last = hash;
+   $(window).on("hashchange", function(){
+        hash = getHash();
+	if (!hash) {
+            window.location.hash = last; 
+	} else if (hash != last) { 
+	    pad.reload(hash);
+        }
+	last = hash;
+   });
 });
+
+var getHash = function() {
+   var hash = window.location.hash
+   return hash ? hash.replace("#", "") : "";
+};
+
+var createGuid = function(){ 
+   return 'xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*36|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(36);
+   });
+};
 
 var padpicker = {};
 padpicker.interval = 1000 * 5;
-padpicker.create = function(id){
-   return new padpicker.PadPicker(id);
+padpicker.create = function(id, firebase_id){
+   return new padpicker.PadPicker(id, firebase_id);
 };
 
-padpicker.PadPicker = function(id) {
-   this.firebase = new Firebase("padpicker.firebaseIO.com");
-   var fbid = window.location.hash.replace("#","");
-   this.firebase = this.firebase.child(fbid);
+padpicker.PadPicker = function(dom_id, firebase_id) {
+   if (!dom_id || !firebase_id) {
+        debugger;
+   }
+   this.firebase = new Firebase("padpicker.firebaseIO.com").child(firebase_id);
 
-   this.codeMirror = CodeMirror(document.getElementById(id), { lineWrapping: true });
+   this.codeMirror = CodeMirror(document.getElementById(dom_id), { lineWrapping: true });
    this.firepad = Firepad.fromCodeMirror(this.firebase, this.codeMirror,
           { richTextShortcuts: true, richTextToolbar: true });
    this.firepad.on('ready', function(){
@@ -25,6 +56,21 @@ padpicker.PadPicker = function(id) {
    });
    this.listenForChange();
    var curr = this;
+   this.firebase.child("file").on("value", function(snapshot){
+	curr.file = snapshot.val();
+   });
+};
+
+padpicker.PadPicker.prototype.reload = function(firebase_id) {
+   this.firepad.dispose();
+
+   this.codeMirror.setValue("");
+   this.firebase = new Firebase("padpicker.firebaseIO.com").child(firebase_id);
+   this.firepad = Firepad.fromCodeMirror(this.firebase, this.codeMirror,
+          { richTextShortcuts: true, richTextToolbar: true });
+   
+   var curr = this;
+   var last = "";
    this.firebase.child("file").on("value", function(snapshot){
 	curr.file = snapshot.val();
    });
